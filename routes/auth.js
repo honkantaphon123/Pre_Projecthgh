5deaaad158cf1bbef1e1b7370a0b33f5e52a10f1
CHOK <6206021611133@fitm.kmutnb.ac.th>
2022-03-31 20:36:29 +0700

    .

--- routes/auth.js
+++ routes/auth.js
@@ -1,36 +1,38 @@
 const router = require("express").Router();
 const User = require("../models/User");
 
+const bcrypt = require("bcrypt");
 // Register API
 router.post("/register", async (req, res) => {
   try {
-   
+    const salt = await bcrypt.genSalt(10);
+    const hashPass = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
       fullname: req.body.fullname,
       username: req.body.username,
       telephone: req.body.telephone,
       email: req.body.email,
-      password: req.body.password,
+      password: hashPass,
     });
     const user = await newUser.save();
     console.log(user);
-    res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 // Login API
-router.post('/login', async (req,res) => {
-    try{
-      const user = await User.findOne({ username: req.body.username })
-      !user && res.status(400).json("Wrong Credentials");
-      const validated = await compare(req.body.password, user.password)
-      validated && res.status (422).json("Incorrect Password")
-      const { password, ...others } = user._doc;// Because we not sending password
-      res.status(200).json(others);
-    } catch(err){
-     res.status(500).json(err);
-    }
- });
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(400).json("Wrong E-mail");
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(422).json("Incorrect Password");
+    const { password, ...others } = user._doc; // Because we not sending password
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
